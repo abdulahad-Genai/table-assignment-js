8eb19f50511373dee4ff480f808b1ad62e6b59f4
abdulahad-Genai <154248193+abdulahad-Genai@users.noreply.github.com>
2024-01-22 19:55:36 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log ("Hello world")
+var inp = prompt("Enter the number")
+
+
+document.write(`${inp} * 1 = `, inp*1 + "<br>")
+document.write(`${inp} * 2 = `, inp*2 + "<br>")
+document.write(`${inp} * 3 = `, inp*3 + "<br>")
+document.write(`${inp} * 4 = `, inp*4 + "<br>")
+document.write(`${inp} * 5 = `, inp*5 + "<br>")
+document.write(`${inp} * 6 = `, inp*6 + "<br>")
+document.write(`${inp} * 7 = `, inp*7 + "<br>")
+document.write(`${inp} * 8 = `, inp*8 + "<br>")
+document.write(`${inp} * 9 = `, inp*9 + "<br>")
+document.write(`${inp} * 10 = `, inp*10 + "<br>")
+
+
